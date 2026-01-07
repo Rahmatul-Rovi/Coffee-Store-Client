e263b685c72ac4d65e0c43e068b8678235585306
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-07 18:35:12 +0600

    Client Section Updated

--- src/component/AddCoffee.jsx
+++ src/component/AddCoffee.jsx
@@ -1,14 +1,121 @@
-import React from 'react';
+import React from "react";
 
 const AddCoffee = () => {
-    return (
-        <div className='p-24'>
-            <div className='p-12 text-center space-y-4'>
-                <h1 className='text-6xl'>Add Coffee</h1>
-            <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
-            </div>
+    const handleAddCoffee = e => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const newCoffee = Object.fromEntries(formData.entries());
+        console.log(newCoffee);
+         
+        //Send coffee data to the DB
+      fetch('http://localhost:3000/coffees',{
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify(newCoffee)
+      })
+      .then(res => res.json())
+      .then(data => {
+        console.log("After adding coffee to DB:",data);
+      })
+
+    }
+  return (
+    <div className="p-24">
+      <div className="p-12 text-center space-y-4">
+        <h1 className="text-6xl">Add Coffee</h1>
+        <p>
+          It is a long established fact that a reader will be distraceted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using Content here.
+        </p>
+      </div>
+
+      <form onSubmit={handleAddCoffee}>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+
+            <label className="label">Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input w-full"
+              placeholder="Enter Coffee Name"
+            />
+              </fieldset>
+        
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+
+            <label className="label">Quantity</label>
+            <input
+              type="text"
+              name="quantity"
+              className="input w-full"
+              placeholder="Enter Coffee Quantity"
+            />
+              </fieldset>
+
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+
+            <label className="label">Supplier</label>
+            <input
+              type="text"
+              name="sufflier"
+              className="input w-full"
+              placeholder="Enter Coffee Supplier"
+            />
+              </fieldset>
+
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+
+            <label className="label">Taste</label>
+            <input
+              type="text"
+              name="taste"
+              className="input w-full"
+              placeholder="Enter Coffee Taste"
+            />
+              </fieldset>
+
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+
+            <label className="label">Category</label>
+            <input
+              type="text"
+              name="category"
+              className="input w-full"
+              placeholder="Enter Coffee Category"
+            />
+              </fieldset>
+
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+
+            <label className="label">Details</label>
+            <input
+              type="text"
+              name="details"
+              className="input w-full"
+              placeholder="Enter Coffee Details"
+            />
+              </fieldset>
         </div>
-    );
+         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border my-6 p-4">
+
+            <label className="label">Photo</label>
+            <input
+              type="text"
+              name="photo"
+              className="input w-full"
+              placeholder="Enter Photo URL"
+            />
+              </fieldset>
+              <input className="btn w-full" type="submit" value="Add Coffee" />
+      </form>
+    </div>
+  );
 };
 
 export default AddCoffee;
