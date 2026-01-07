646fc34a52235c64e6419e894d85404c89ea962e
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-08 00:08:30 +0600

    Client Section Updated

--- src/component/CoffeeCard.jsx
+++ src/component/CoffeeCard.jsx
@@ -16,14 +16,20 @@ const CoffeeCard = ({ coffee }) => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-       
-
-
-        // Swal.fire({
-        //   title: "Deleted!",
-        //   text: "Your file has been deleted.",
-        //   icon: "success",
-        // });
+        //Start deleting the coffee
+        fetch(`http://localhost:3000/coffees/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your Coffee has been deleted.",
+                icon: "success",
+              });
+            }
+          })
       }
     });
   };
